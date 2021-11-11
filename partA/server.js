81686e66eeba73cb4e8dc32846f3e004d9fd1a3b
Henry <hfang49@yahoo.ca>
2021-11-11 13:48:05 -0600

    Added html pages. Added registration fro staff.

--- partA/server.js
+++ partA/server.js
@@ -24,9 +24,40 @@ app.get('/connect', (req, res) => {
 
 		console.log("Connected!");
 	});
+});
+
+app.post('/registerstaff', (req, res) => {
+	var file_output = "INSERT INTO staff (firstname, lastname, role, joindate) VALUES ('" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.role + "', '" + new Date() + "');";
+	console.log(file_output);
+	config.query(file_output, function (err, result) {
+		if (err) {
+			console.log(err);
+		}
+		res.send("Registration received!");
+	});
+});
+
+app.post('/refreshstaff', (req,res) => {
+	var response = new Object();
+	if (req.body == undefined) {
+		response.answer = "No staff!";
+	}
+	else {
+		var query_string = "SELECT * FROM staff;";
+		console.log(query_string);
+
+		config.query(query_string, function (err, result) {
+			if (err) {
+				console.log(err);
+				response.answer = "No staff!";
+				res.send(JSON.stringify(response));
+			}
+			var resultArray = JSON.parse(JSON.stringify(result));
+			response.answer = resultArray;
+			res.send(JSON.stringify(response));
+		});
+	}
 })
-
-
 
 
 app.use('/', express.static('pages'));
